6dce0eca9f93b74820d943b1b011a3e40f7b7f56
Sourav-Goyal19 <goyalsourav935@gmail.com>
2024-03-23 23:33:43 +0530

    New update

--- client/components/Avatar.tsx
+++ client/components/Avatar.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-const Avatar = ({ image }: { image: any }) => {
+const Avatar = ({ image, online }: { image: any; online?: boolean }) => {
   return (
     <div className="relative">
       <div className="relative rounded-full inline-block overflow-hidden h-9 w-9 md:h-11 md:w-11">
@@ -12,7 +12,9 @@ const Avatar = ({ image }: { image: any }) => {
           sizes="100%"
         />
       </div>
-      <span className="absolute block top-0 right-0 bg-green-500 ring-2 ring-white rounded-full h-2 w-2 md:h-3 md:w-3" />
+      {online && (
+        <span className="absolute block top-0 right-0 bg-green-500 ring-2 ring-white rounded-full h-2 w-2 md:h-3 md:w-3" />
+      )}
     </div>
   );
 };
