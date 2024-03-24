c677926f3d3dbfffbbcf900ade1ddbd14f2c7289
Sourav-Goyal19 <goyalsourav935@gmail.com>
2024-03-24 16:36:30 +0530

    Update

--- client/hooks/useRoutes.ts
+++ client/hooks/useRoutes.ts
@@ -6,17 +6,22 @@ import { HiArrowLeftOnRectangle, HiUsers } from "react-icons/hi2";
 import { useMemo } from "react";
 import axios from "axios";
 import toast from "react-hot-toast";
+import { useAppSelector } from "@/redux/hooks";
+import { useDispatch } from "react-redux";
+import { setUser } from "@/redux/slices/user/user/userSlice";
 
 const useRoutes = () => {
   const pathname = usePathname();
   const router = useRouter();
   const { conversationId } = useConversation();
+  const dispatch = useDispatch();
 
   const signOut = () => {
     axios
       .get(`${process.env.NEXT_PUBLIC_SERVER_URL}/auth/logout`)
       .then((res) => {
         if (res.status === 200) {
+          dispatch(setUser(null));
           router.push("/");
           toast.success(res.data.msg);
         }
